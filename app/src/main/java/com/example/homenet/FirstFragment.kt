580a62ed90a265bec95f7c4745c72e8e8cafa490
Badr <badrkmc@gmail.com>
2022-09-13 16:34:26 +0500

    fix redundant imports

--- app/src/main/java/com/example/homenet/FirstFragment.kt
+++ app/src/main/java/com/example/homenet/FirstFragment.kt
@@ -1,17 +1,14 @@
 package com.example.homenet
 
-import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.example.homenet.databinding.FragmentFirstBinding
 import com.example.homenet.utils.Util
-import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 
 /**
--- app/src/main/java/com/example/homenet/services/LocationService.kt
+++ app/src/main/java/com/example/homenet/services/LocationService.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.*
-import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
@@ -42,6 +41,7 @@ class LocationService : Service() {
   private fun startTimer() {
     var timer: Timer? = Timer()
     val timerTask = object : TimerTask() {
+      @RequiresApi(Build.VERSION_CODES.M)
       override fun run() {
         try {
           if (Util.arePointsNear(
